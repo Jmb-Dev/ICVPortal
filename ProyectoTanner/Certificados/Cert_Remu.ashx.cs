78df4818de0877b56c99386a917091686ebf69b3
jfds-dev <jfds.205@outlook.com>
2019-10-21 18:59:09 -0300

    21102019 cert_remu

--- ProyectoTanner/Certificados/Cert_Remu.ashx.cs
+++ ProyectoTanner/Certificados/Cert_Remu.ashx.cs
@@ -10,12 +10,37 @@ namespace ProyectoTanner.Certificados
     /// </summary>
     public class Cert_Remu : IHttpHandler
     {
-
         public void ProcessRequest(HttpContext context)
         {
-            context.Response.ContentType = "text/plain";
-            context.Response.Write("Hola a todos");
+            var rut = context.Request.QueryString["Rut"];
+            var rem = "";
+            var mot = "XXXXXX";
+            string Token = "Bearer  " + context.Request.QueryString["Tok"];
+
+            var url = "http://164.77.177.179:5055/api/ZHR_MF_REN_ANT?RUT=" + rut + "&REM=" + rem + "&MOT=" + mot + "";
+
+            var json2 = new WebClient();
+            json2.Headers.Add("Authorization", Token);
+            var result = json2.DownloadString(url);
+
+            JObject json = JObject.Parse(result);
+
+            Byte[] bytes2 = null;
+
+            for (int i = 0; i < json["RENTA"][0]["E_PDF"].Count(); i++)
+            {
+                bytes2 = (Byte[])json["RENTA"][0]["E_PDF"][i].SelectToken("E_PDF");
+            }
+
+            context.Response.Buffer = true;
+            context.Response.Charset = "";
+            context.Response.Cache.SetCacheability(HttpCacheability.NoCache);
+            context.Response.ContentType = "application/pdf";
+            context.Response.BinaryWrite(bytes2);
+            context.Response.Flush();
+
         }
+
 
         public bool IsReusable
         {
