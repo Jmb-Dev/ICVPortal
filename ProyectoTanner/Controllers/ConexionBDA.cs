a1876eb95cf7ab02b392859dacaf0f017a12978f
jfds-dev <jfds.205@outlook.com>
2019-10-22 22:50:00 -0300

    normalizacion
    ultimo commit 22.10.2019
    22:49

---
+++ ProyectoTanner/Controllers/ConexionBDA.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.IO;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Web.Mvc;
+
+
+namespace ProyectoTanner.Controllers
+{
+    public class ConexionBDA : ApiController
+    {
+        string conn = ConfigurationManager.ConnectionString["DBA_ICV"].ConnectionString;
+        SqlConnection sql = new SqlConnection(conn);
+    }
+
+
+
+       
+       
+}
